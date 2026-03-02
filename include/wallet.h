1919d7ace6b0ef9563539e9e33b64c430c59d9ba
arinchikzhuzhik <arinazhuzhoma4@gmail.com>
2026-03-02 20:12:49 +0300

    add class wallet

---
+++ include/wallet.h
@@ -0,0 +1,13 @@
+//
+// Created by Arina on 02.03.2026.
+//
+
+#ifndef GAME_WALLET_H
+#define GAME_WALLET_H
+
+
+class wallet {
+};
+
+
+#endif //GAME_WALLET_H
---
+++ src/wallet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Arina on 02.03.2026.
+//
+
+#include "wallet.h"
